266795450ad9050f6c220547bda032f48c142586
Salam <salamwaddah@gmail.com>
2021-03-10 14:03:35 +0400

    upgrade to sympony 5

--- lib/PHPExiftool/Exiftool.php
+++ lib/PHPExiftool/Exiftool.php
@@ -48,10 +48,10 @@ class Exiftool implements LoggerAwareInterface
     public function executeCommand($command, $timeout = 60)
     {
         $command = ($this->binaryPath == null? self::getBinary(): $this->binaryPath) . ' ' . $command;
-        $process = new Process($command);
+        $process = Process::fromShellCommandline($command);
         $process->setTimeout($timeout);
 
-        $this->logger->addInfo(sprintf('Exiftool executes command %s', $process->getCommandLine()));
+        $this->logger->info(sprintf('Exiftool executes command %s', $process->getCommandLine()));
 
         $process->run();
 
